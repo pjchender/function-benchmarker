da03ca47994eec9d1906051041148b4ce0c6415c
pjchender <pjchender@gmail.com>
2020-02-11 22:37:21 +0800

    fix: remove redundant commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// index.js
 const benchmarker = (testFunction, times = 1000000) => {
   if (typeof testFunction !== 'function') {
     throw new Error('Did not provide a valid function for test.');
